03bd0771fc17f13dbc4b4d0e25b11d0793348e36
liuyang19900520 <liuyang19900520@hotmail.com>
2020-01-13 14:11:45 +0900

    update base

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,6 @@
   module.exports = {
     title: "LiuYang's Blog",
     description: '我常常想，想进步总不是坏事',
-    base: "/docs/",
     themeConfig: {
       logo: '/assets/img/logo.png',
       search: false,

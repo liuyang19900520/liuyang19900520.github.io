96a04d59432847b192272246c090592aacb1e3a7
liuyang19900520 <liuyang19900520@hotmail.com>
2020-02-29 23:58:50 +0900

    NestJs 收藏夹创建

--- docs/.vuepress/config/sidebar/blog.js
+++ docs/.vuepress/config/sidebar/blog.js
@@ -7,6 +7,14 @@ module.exports = [{
     ]
   },
   {
+    title: 'NestJS', // 必要的
+    collection: false,
+    sidebarDepth: 1, // 可选的, 默认值是 1
+    children: [
+      'nestjs/nestjs-collection'
+    ]
+  },
+  {
     title: 'Frontend', // 必要的
     collection: false,
     sidebarDepth: 1, // 可选的, 默认值是 1
